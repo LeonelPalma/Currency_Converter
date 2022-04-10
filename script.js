088da298d0a230dfcf5b8632da5bba3afe8e5524
Leonel Palma <80472480+LeonelPalma@users.noreply.github.com>
2022-04-10 16:29:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+<-- To Real -->
+function Converter() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  var valorEmDolarNumerico = parseFloat(valor);
+
+  var valorEmReal = valorEmDolarNumerico * 5;
+
+  //console.log(valorEmReal);
+
+  var elementoValorConvertido = document.getElementById("valorConvertido");
+  document.getElementById("valorConvertido");
+  var valorConvertido = "O Resultado em Real é R$: " + valorEmReal;
+  elementoValorConvertido.innerHTML = valorConvertido;
+}
+
+<!--
+<-- To Euro -->
 function Converter() {
   var valorElemento = document.getElementById("valor");
   var valor = valorElemento.value;
@@ -5,10 +23,11 @@ function Converter() {
 
   var valorEmEuro = valorEmDolarNumerico * 5;
 
-  //console.log(valorEmReal);
+  //console.log(valorEmEuro);
 
   var elementoValorConvertido = document.getElementById("valorConvertido");
   document.getElementById("valorConvertido");
   var valorConvertido = "O Resultado em Euros é: " + valorEmReal "€";
   elementoValorConvertido.innerHTML = valorConvertido;
 }
+-->
