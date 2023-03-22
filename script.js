b7c0755529d210a27721af1b9f71e1375fe36106
Leonel Palma <80472480+LeonelPalma@users.noreply.github.com>
2023-03-22 18:59:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
-<!-- To Real -->
-function Converter() {
-  var valorElemento = document.getElementById("valor");
-  var valor = valorElemento.value;
-  var valorEmDolarNumerico = parseFloat(valor);
-
-  var valorEmReal = valorEmDolarNumerico * 5;
-
-  console.log(valorEmReal);
-
-  var elementoValorConvertido = document.getElementById("valorConvertido");
-  document.getElementById("valorConvertido");
-  var valorConvertido = "O Resultado em Real é R$: " + valorEmReal;
-  elementoValorConvertido.innerHTML = valorConvertido;
+// Include api for currency change
+const api = "https://api.exchangerate-api.com/v4/latest/USD";
+  
+// For selecting different controls
+var search = document.querySelector(".searchBox");
+var convert = document.querySelector(".convert");
+var fromCurrecy = document.querySelector(".from");
+var toCurrecy = document.querySelector(".to");
+var finalValue = document.querySelector(".finalValue");
+var finalAmount = document.getElementById("finalAmount");
+var resultFrom;
+var resultTo;
+var searchValue;
+  
+// Event when currency is changed
+fromCurrecy.addEventListener('change', (event) => {
+    resultFrom = `${event.target.value}`;
+});
+  
+// Event when currency is changed
+toCurrecy.addEventListener('change', (event) => {
+    resultTo = `${event.target.value}`;
+});
+  
+search.addEventListener('input', updateValue);
+  
+// Function for updating value
+function updateValue(e) {
+    searchValue = e.target.value;
 }
-
-
-<!-- To Euro -->
-function Converter() {
-  var valorElemento = document.getElementById("valor");
-  var valor = valorElemento.value;
-  var valorEmDolarNumerico = parseFloat(valor);
-
-  var valorEmEuro = valorEmDolarNumerico * 5;
-
-  console.log(valorEmEuro);
-
-  var elementoValorConvertido = document.getElementById("valorConvertido");
-  document.getElementById("valorConvertido");
-  var valorConvertido = "O Resultado em Euros é: " + valorEmReal "€";
-  elementoValorConvertido.innerHTML = valorConvertido;
+  
+// When user clicks, it calls function getresults 
+convert.addEventListener("click", getResults);
+  
+// Function getresults
+function getResults() {
+    fetch(`${api}`)
+        .then(currency => {
+            return currency.json();
+        }).then(displayResults);
 }
-
+  
+// Display results after conversion
+function displayResults(currency) {
+    let fromRate = currency.rates[resultFrom];
+    let toRate = currency.rates[resultTo];
+    finalValue.innerHTML = 
+       ((toRate / fromRate) * searchValue).toFixed(2);
+    finalAmount.style.display = "block";
+}
+  
+// When user click on reset button
+function clearVal() {
+    window.location.reload();
+    document.getElementsByClassName("finalValue").innerHTML = "";
+};
